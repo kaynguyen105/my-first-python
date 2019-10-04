edb1d1b0307162b353969cfc1f6aa1a6f89af273
kaynguyen105 <56107665+kaynguyen105@users.noreply.github.com>
2019-10-04 16:22:38 +0700

    Update enums.py

--- enums.py
+++ enums.py
@@ -1,3 +1,5 @@
 class Session(object):
-    sessions = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: "six", 7: "seven", 8: "eight", 9: "nine"}
-    star = {'one': 1, 'two': 2, 'three': 3, 'four': 4}
+    NUMBER_TO_TEXT_MAP = {
+        "SESSIONS": {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: "six", 7: "seven", 8: "eight", 9: "nine"},
+        "STARS": {'one': 1, 'two': 2, 'three': 3, 'four': 4}
+    }
