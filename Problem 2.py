fa9c1200e78da8c4e82e7a449b3efc254b033e0c
kaynguyen105 <56107665+kaynguyen105@users.noreply.github.com>
2019-10-08 15:02:39 +0700

    Update Problem 2.py

--- Problem 2.py
+++ Problem 2.py
@@ -8,21 +8,29 @@ class Step:
         self.number_of_stars = number_of_stars
 
     def make_step(self):
+        try:
             if (self.number_of_sessions in Session.NUMBER_TO_TEXT_MAP["SESSIONS"].keys()) and \
-                    (self.number_of_stars in Session.NUMBER_TO_TEXT_MAP["STARS"].keys()):
-                print("I completed "
-                      + Session.NUMBER_TO_TEXT_MAP["SESSIONS"][self.number_of_sessions]
-                      + " sessions and I rated my expert "
-                      + str(Session.NUMBER_TO_TEXT_MAP["STARS"][self.number_of_stars])
-                      + " starts")
+                        (self.number_of_stars in Session.NUMBER_TO_TEXT_MAP["STARS"].keys()):
+                    print("I completed "
+                          + Session.NUMBER_TO_TEXT_MAP["SESSIONS"][self.number_of_sessions]
+                          + " sessions and I rated my expert "
+                          + str(Session.NUMBER_TO_TEXT_MAP["STARS"][self.number_of_stars])
+                          + " starts")
+            else:
+                raise InvalidValueException
+        except:
             if self.number_of_sessions not in Session.NUMBER_TO_TEXT_MAP["SESSIONS"].keys():
-                print("Invalid number of sessions")
+                print(InvalidValueException(message1))
             elif self.number_of_stars not in Session.NUMBER_TO_TEXT_MAP["STARS"].keys():
-                print("Invalid number of starts")
+                print(InvalidValueException(message2))
 
 
-Step(9, "four").make_step()
+class InvalidValueException(Exception):
+    def __init__(self, message):
+        self.message = message
 
 
-a = Step(9, "five")
-a.make_step()
+message1 = "Invalid number of sessions"
+message2 = "Invalid number of stars"
+Step(8, "four").make_step()
+
