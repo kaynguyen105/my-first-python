a05ff7a0dcae2bb8c26a3e9e99d84757d01528f1
kaynguyen105 <56107665+kaynguyen105@users.noreply.github.com>
2019-10-09 08:39:47 +0700

    Update Problem 4.py

--- Problem 4.py
+++ Problem 4.py
@@ -8,14 +8,19 @@ class StorefrontConfig:
         self.data = data
 
     def update(self, modify_data):
-        for y in self.data and modify_data:
-            if self.data[y] != modify_data[y]:
-                self.data[y] = str(modify_data[y])
+        update_helper(self.data, modify_data)
 
 
+def update_helper(data, modify_data):
+    for key, value in modify_data.items():
+        if key in data.keys() and not isinstance(value, dict):
+            data[key] = value
+        else:
+            data[key] = update_helper(data[key], value)
+    return data
 
 
-class FileController():
+class FileController:
 
     def read_file(self, location):
         """
@@ -35,8 +40,11 @@ class FileController():
         :return:
         """
         my_file = open("/Users/hainguyen/Documents/workspace_python/PythonTutorial/Modules/file_name.json", "w")
-        my_file.write(str(storefront_config.data))
+        my_file.write(json.dumps(storefront_config.data))
         my_file.close()
+
+
+
 
 file_controller = FileController()
 config= file_controller.read_file("/Users/hainguyen/Documents/workspace_python/PythonTutorial/Modules/data.json")
